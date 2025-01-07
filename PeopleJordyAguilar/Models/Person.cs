90bb3398ab85ba1f37e1d2ec0cfa10fca8de38f9
Jordy Aguilar <jeay022@gmail.com>
2025-01-07 16:06:42 -0500

    Cree clase person

---
+++ PeopleJordyAguilar/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleJordyAguilar.Models
+{
+    [Table("Persona")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
